1e42316dd4e8a1c4c3335cf63a76ad106f53613e
Neneng Nurhamidah <xsis.nenengn@xl.co.id>
2018-12-27 09:35:50 +0700

    [final]

--- src/main/java/pageservice/SidebarPageConfigAjaxBasedImpl.java
+++ src/main/java/pageservice/SidebarPageConfigAjaxBasedImpl.java
@@ -9,7 +9,7 @@ public class SidebarPageConfigAjaxBasedImpl implements SidebarPageConfig{
 	
 	HashMap<String,SidebarPage> pageMap = new LinkedHashMap<String,SidebarPage>();
 	public SidebarPageConfigAjaxBasedImpl(){
-		pageMap.put("fn1",new SidebarPage("user","User","/imgs/fn.png","/member/member.zul"));
+		pageMap.put("fn1",new SidebarPage("member","Member","/imgs/fn.png","/member/member.zul"));
 		pageMap.put("fn2",new SidebarPage("deployment","Deployment","/imgs/fn.png","/deployment/deployment.zul"));
 		pageMap.put("fn3",new SidebarPage("tester","Generate KPI","/imgs/fn.png","/tester/tester.zul"));
 		pageMap.put("fn4",new SidebarPage("testerList","Tester KPI","/imgs/fn.png","/tester/testerList.zul"));
--- src/main/java/vmd/AttendanceVmd.java
+++ src/main/java/vmd/AttendanceVmd.java
@@ -1,15 +1,24 @@
 package vmd;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JFileChooser;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zhtml.Fileupload;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Messagebox;
 
 import core.dto.AttendanceDto;
+import core.dto.MemberDto;
 import util.BaseUri;
 import util.JsonUtil;
 import util.RestResponse;
@@ -22,25 +31,66 @@ public class AttendanceVmd {
 	private final String WS_URL = uri.BaseUriParam();
 	
 	private AttendanceDto attendanceDto = new AttendanceDto();
+	private AttendanceDto attendanceDto1 = new AttendanceDto();
 	private List<AttendanceDto> listAttendanceDto = new ArrayList<>();
 	
+	private MemberDto memberDto = new MemberDto();
+	private List<MemberDto> listMemberDto = new ArrayList<>();
+	
+	private String username;
 	
 	@Init
 	public void load(){
-		String uri = WS_URL + "/attendance/all";
+		String uri = WS_URL + "/attendance/allWithMember";
 		RestResponse restResponse = new RestResponse();
 		BaseVmd bs = new BaseVmd();
 		
+		String uriMember = WS_URL + "/member/allWithMember";
+		RestResponse restResponseMember = new RestResponse();
+		BaseVmd bsMember = new BaseVmd();
+//		
 		try {
 			restResponse = bs.callWs(uri,null,HttpMethod.GET);
 			System.out.println("check response : "+restResponse.getContents());
 			listAttendanceDto = JsonUtil.mapJsonToListObject(restResponse.getContents(), AttendanceDto.class);
+			
+			restResponseMember = bsMember.callWs(uriMember,null,HttpMethod.GET);
+			System.out.println("check response : "+restResponseMember.getContents());
+			listMemberDto = JsonUtil.mapJsonToListObject(restResponseMember.getContents(), MemberDto.class);
+			
 		} catch (Exception e) {
 			System.out.println("Unable to convert JSON!");
 			logger.error(e.getMessage());
 			e.printStackTrace();
 		}
 		//setIndexHdr(listMemberDto.size()+1);
+	}
+	
+	
+	@Command("select")
+	@NotifyChange("listAttendanceDto")
+	public void select() {
+		Clients.showNotification(username, Clients.NOTIFICATION_TYPE_INFO, null, null, 2000);
+		String uri = WS_URL + "/attendance/allWithName";
+		RestResponse restResponse = new RestResponse();
+		BaseVmd bs = new BaseVmd();
+		attendanceDto1.setNik(username);
+		
+		try {
+			restResponse = bs.callWs(uri,attendanceDto1,HttpMethod.POST);
+			System.out.println("check response : "+restResponse.getContents());
+			listAttendanceDto = JsonUtil.mapJsonToListObject(restResponse.getContents(), AttendanceDto.class);
+			
+//			restResponseMember = bsMember.callWs(uriMember,null,HttpMethod.GET);
+//			System.out.println("check response : "+restResponseMember.getContents());
+//			listMemberDto = JsonUtil.mapJsonToListObject(restResponseMember.getContents(), MemberDto.class);
+			
+		} catch (Exception e) {
+			System.out.println("Unable to convert JSON!");
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
@@ -53,8 +103,38 @@ public class AttendanceVmd {
 	public List<AttendanceDto> getListAttendanceDto() {
 		return listAttendanceDto;
 	}
-	public void setListMemberDto(List<AttendanceDto> listAttendanceDto) {
+	public void setListAttendanceDto(List<AttendanceDto> listAttendanceDto) {
 		this.listAttendanceDto = listAttendanceDto;
+	}
+
+
+	public MemberDto getMemberDto() {
+		return memberDto;
+	}
+
+
+	public void setMemberDto(MemberDto memberDto) {
+		this.memberDto = memberDto;
+	}
+
+
+	public List<MemberDto> getListMemberDto() {
+		return listMemberDto;
+	}
+
+
+	public void setListMemberDto(List<MemberDto> listMemberDto) {
+		this.listMemberDto = listMemberDto;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	
 	
